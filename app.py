f6e22d830f3fbfede260868b2e937a9accc442a0
Park_hs <qwe040629@gmail.com>
2026-06-24 17:34:03 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,7 @@
+from flask import Flask, render_template
+app = Flask(__name__)#이 파일 위치로 templates, static 파일의 위치를 찾겠다
+@app.route("/") #데코레이터 기본 주소"/"로 접속 시 다음과 같은 함수 실행
+def index() :
+    return render_template("index.html")
+if __name__ == "__main__" :
+    app.run(debug=True)
